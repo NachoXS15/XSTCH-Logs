1f2264d1d1befafa706ecea982e5e9bad6d3c1c7
Ignacio Joaquín Pantoja <pantojaignacio642@gmail.com>
2025-02-01 11:49:22 -0300

    fixed navbar

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 export default function layout({children}: {children: React.ReactNode}) {
   return (
-    <main className="w-full min-h-screen overflow-x-hidden flex bg-white text-black font-[family-name:var(--font-poppins)]">
+    <main className="w-full min-h-screen overflow-x-hidden flex flex-col-reverse md:flex-row md:justify-start justify-between bg-white text-black font-[family-name:var(--font-poppins)]">
         <Sidebar />
         <main className="w-full flex items-start justify-center">
             {children}
